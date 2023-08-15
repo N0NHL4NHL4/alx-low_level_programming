99d7fcbe1a046c0dbedea707a6e395c4c7f7bc7d
N0NHL4NHL4 <1889514@students.wits.ac.za>
2023-08-14 18:49:08 -0700

    uses puts to print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
   *main - it prints out output using puts
   *
@@ -8,6 +7,6 @@
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
